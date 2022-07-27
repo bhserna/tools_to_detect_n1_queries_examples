66b3f4188c2cff006e0f9dc9f88b6623ffb3f522
bhserna <bhserna@gmail.com>
2022-07-27 18:37:07 -0500

    Strict loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
   def index
+    # Without strict loading
     @posts = Post.limit(5)
+
+    # With strict loading
+    # -------------------
+    # Uncomment to see the error
+    # @posts.strict_loading!
   end
 end
